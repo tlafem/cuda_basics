257c93bb5206b421fa325a65da8174fcdd3c7ce2
edlafem1 <edlafem1@umbc.edu>
2015-10-31 17:58:58 -0400

    Documentation added. Vector macros reworked.

--- source/add.cuh
+++ source/add.cuh
@@ -9,6 +9,12 @@
 #include <stdio.h>
 #include <math.h>
 
+/**
+Componentwise addition of two vectors a and b, storing the result in a vector c.
+Vectors must all be of the same length.
+
+Returns: 0 on success, non-zero otherwise
+*/
 int add_dVectors(dVector a, dVector b, dVector c);
 
 void test_add(int n);
